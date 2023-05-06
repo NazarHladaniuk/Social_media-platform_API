3f4896c95d9016eb19f9620597420447342f1851
Nazar Hladaniuk <powerlolek46@gmail.com>
2023-05-06 20:44:38 +0300

    relocate secret key to .env

--- app/settings.py
+++ app/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -20,9 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-0)&g!_fk1he=wfz#85o$zym!vcvlql^&k9=r(nxud__o&kxs$("
-)
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
